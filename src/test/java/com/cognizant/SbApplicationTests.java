40753b2aec34f5c6c3fe716660ddc9684ea9c373
jayalakshmicherman <jayalakshmichermadurai@gmail.com>
2021-01-15 10:58:02 +0530

    SB-APPLICATION Structure Project

---
+++ src/test/java/com/cognizant/SbApplicationTests.java
@@ -0,0 +1,17 @@
+package com.cognizant;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class SbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
